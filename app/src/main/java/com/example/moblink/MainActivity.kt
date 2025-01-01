27a5a9ffabd2f111da77b16561494e29dc0b78cc
Erik Moqvist <erik.moqvist@gmail.com>
2025-01-01 21:00:52 +0100

    Websocket started.

--- app/src/main/java/com/example/moblink/MainActivity.kt
+++ app/src/main/java/com/example/moblink/MainActivity.kt
@@ -15,17 +15,47 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.Color
 import com.example.moblink.ui.theme.MoblinkTheme
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import kotlin.concurrent.thread
+import okhttp3.WebSocket
+import okhttp3.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
+    private var webSocket: WebSocket? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         requestNetwork(NetworkCapabilities.TRANSPORT_CELLULAR, "Cellular")
         requestNetwork(NetworkCapabilities.TRANSPORT_WIFI, "WiFi")
+        val okHttpClient = OkHttpClient.Builder().pingInterval(30, TimeUnit.SECONDS).build()
+        val request = Request.Builder().url("ws://192.168.50.203:7777").build()
+        webSocket = okHttpClient.newWebSocket(request, object : WebSocketListener() {
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                Log.i("Moblink","Websocket opened")
+                super.onOpen(webSocket, response)
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                Log.i("Moblink","Websocket message received: $text")
+                super.onMessage(webSocket, text)
+            }
+
+            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                Log.i("Moblink","Websocket closed $reason (code $code)")
+                super.onClosed(webSocket, code, reason)
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                Log.i("Moblink","Websocket failure $t")
+                super.onFailure(webSocket, t, response)
+            }
+        })
         setContent {
             MoblinkTheme {
                 Surface(color = Color.Cyan) {
