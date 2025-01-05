97b644bad40c9c1879a797f0f5220e80ceec7640
Erik Moqvist <erik.moqvist@gmail.com>
2025-01-05 13:44:16 +0100

    Suppress lint.

--- app/src/main/java/com/eerimoq/moblink/WakeLock.kt
+++ app/src/main/java/com/eerimoq/moblink/WakeLock.kt
@@ -1,11 +1,13 @@
 package com.eerimoq.moblink
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.PowerManager
 
 class WakeLock {
     private var wakeLock: PowerManager.WakeLock? = null
 
+    @SuppressLint("WakelockTimeout")
     fun acquire(context: Context) {
         val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
         wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Moblink:lock")
