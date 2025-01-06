07beec9902bb632263860a3b8f0e4b7b0cfdbb7a
Erik Moqvist <erik.moqvist@gmail.com>
2025-01-06 21:15:23 +0100

    Settings for up to five streamers.

--- app/src/main/java/com/eerimoq/moblink/MainActivity.kt
+++ app/src/main/java/com/eerimoq/moblink/MainActivity.kt
@@ -8,13 +8,25 @@ import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.os.BatteryManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -28,19 +40,36 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.eerimoq.moblink.ui.theme.MoblinkTheme
 
 class MainActivity : ComponentActivity() {
-    private val relay = Relay()
+    private val relays = arrayOf(Relay(), Relay(), Relay(), Relay(), Relay())
     private var settings: Settings? = null
-    private var started = false
     private var version = "?"
-    private val buttonText = mutableStateOf("Start")
-    private val status = mutableStateOf("")
+    private val buttonTexts =
+        arrayOf(
+            mutableStateOf("Start"),
+            mutableStateOf("Start"),
+            mutableStateOf("Start"),
+            mutableStateOf("Start"),
+            mutableStateOf("Start"),
+        )
+    private val statuses =
+        arrayOf(
+            mutableStateOf(""),
+            mutableStateOf(""),
+            mutableStateOf(""),
+            mutableStateOf(""),
+            mutableStateOf(""),
+        )
+    private var relaysStarted = arrayOf(false, false, false, false, false)
     private val wakeLock = WakeLock()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,21 +80,26 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onDestroy() {
-        stop()
+        for (relayIndex in 0..4) {
+            stop(relayIndex)
+        }
         super.onDestroy()
     }
 
     private fun setup() {
         wakeLock.setup(this)
         settings = Settings(getSharedPreferences("settings", Context.MODE_PRIVATE))
-        relay.setup(
-            settings!!.relayId,
-            settings!!.streamerUrl,
-            settings!!.password,
-            settings!!.name,
-            { status -> runOnUiThread { this.status.value = status } },
-            { callback -> getBatteryPercentage(callback) },
-        )
+        val database = settings!!.database
+        for (relayIndex in 0..4) {
+            relays[relayIndex].setup(
+                database.relayId,
+                database.relays[relayIndex].streamerUrl,
+                database.relays[relayIndex].password,
+                database.name,
+                { status -> runOnUiThread { this.statuses[relayIndex].value = status } },
+                { callback -> getBatteryPercentage(callback) },
+            )
+        }
         try {
             val packageInfo = packageManager.getPackageInfo(packageName, 0)
             version = packageInfo.versionName
@@ -76,32 +110,38 @@ class MainActivity : ComponentActivity() {
 
     private fun saveSettings() {
         settings!!.store()
-        relay.updateSettings(
-            settings!!.relayId,
-            settings!!.streamerUrl,
-            settings!!.password,
-            settings!!.name,
-        )
+        for (relayIndex in 0..4) {
+            relays[relayIndex].updateSettings(
+                settings!!.database.relayId,
+                settings!!.database.relays[relayIndex].streamerUrl,
+                settings!!.database.relays[relayIndex].password,
+                settings!!.database.name,
+            )
+        }
     }
 
-    private fun start() {
-        if (started) {
-            return
+    private fun start(relayIndex: Int) {
+        if (!isStarted()) {
+            Log.i("Moblink", "Start")
+            startService(this)
+            wakeLock.acquire()
         }
-        started = true
-        startService(this)
-        wakeLock.acquire()
-        relay.start()
+        relaysStarted[relayIndex] = true
+        relays[relayIndex].start()
     }
 
-    private fun stop() {
-        if (!started) {
-            return
+    private fun stop(relayIndex: Int) {
+        relaysStarted[relayIndex] = false
+        if (!isStarted()) {
+            Log.i("Moblink", "Stop")
+            stopService(this)
+            wakeLock.release()
         }
-        started = false
-        stopService(this)
-        wakeLock.release()
-        relay.stop()
+        relays[relayIndex].stop()
+    }
+
+    private fun isStarted(): Boolean {
+        return relaysStarted.contains(true)
     }
 
     private fun getBatteryPercentage(callback: (Int) -> Unit) {
@@ -126,12 +166,16 @@ class MainActivity : ComponentActivity() {
         return object : NetworkCallback() {
             override fun onAvailable(network: Network) {
                 super.onAvailable(network)
-                relay.setCellularNetwork(network)
+                for (relayIndex in 0..4) {
+                    relays[relayIndex].setCellularNetwork(network)
+                }
             }
 
             override fun onLost(network: Network) {
                 super.onLost(network)
-                relay.setCellularNetwork(null)
+                for (relayIndex in 0..4) {
+                    relays[relayIndex].setCellularNetwork(null)
+                }
             }
         }
     }
@@ -140,23 +184,24 @@ class MainActivity : ComponentActivity() {
         return object : NetworkCallback() {
             override fun onAvailable(network: Network) {
                 super.onAvailable(network)
-                relay.setWiFiNetwork(network)
+                for (relayIndex in 0..4) {
+                    relays[relayIndex].setWiFiNetwork(network)
+                }
             }
 
             override fun onLost(network: Network) {
                 super.onLost(network)
-                relay.setWiFiNetwork(null)
+                for (relayIndex in 0..4) {
+                    relays[relayIndex].setWiFiNetwork(null)
+                }
             }
         }
     }
 
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     fun Main() {
-        var streamerUrlInput by remember { mutableStateOf(settings!!.streamerUrl) }
-        var passwordInput by remember { mutableStateOf(settings!!.password) }
-        var nameInput by remember { mutableStateOf(settings!!.name) }
-        val text by buttonText
-        val status by status
+        var nameInput by remember { mutableStateOf(settings!!.database.name) }
         val focusManager = LocalFocusManager.current
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -169,35 +214,10 @@ class MainActivity : ComponentActivity() {
                 contentDescription = null,
             )
             OutlinedTextField(
-                value = streamerUrlInput,
-                onValueChange = {
-                    streamerUrlInput = it
-                    settings!!.streamerUrl = streamerUrlInput
-                    saveSettings()
-                },
-                label = { Text("Streamer URL") },
-                placeholder = { Text("ws://192.168.0.10:7777") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
-            )
-            OutlinedTextField(
-                value = passwordInput,
-                onValueChange = {
-                    passwordInput = it
-                    settings!!.password = passwordInput
-                    saveSettings()
-                },
-                label = { Text("Password") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
-            )
-            OutlinedTextField(
                 value = nameInput,
                 onValueChange = {
                     nameInput = it
-                    settings!!.name = nameInput
+                    settings!!.database.name = nameInput
                     saveSettings()
                 },
                 label = { Text("Name") },
@@ -205,19 +225,75 @@ class MainActivity : ComponentActivity() {
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
             )
-            Text(status)
-            Button(
-                onClick = {
-                    if (!started) {
-                        buttonText.value = "Stop"
-                        start()
-                    } else {
-                        buttonText.value = "Start"
-                        stop()
+            val pagerState = rememberPagerState(pageCount = { 5 })
+            Row(
+                Modifier.wrapContentHeight()
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 20.dp),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                repeat(pagerState.pageCount) { iteration ->
+                    val color =
+                        if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                    Box(
+                        modifier =
+                            Modifier.padding(2.dp).clip(CircleShape).background(color).size(10.dp)
+                    )
+                }
+            }
+            HorizontalPager(state = pagerState) { relayIndex ->
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    val relay = settings!!.database.relays[relayIndex]
+                    var streamerUrlInput by remember { mutableStateOf(relay.streamerUrl) }
+                    var passwordInput by remember { mutableStateOf(relay.password) }
+                    val status by statuses[relayIndex]
+                    val text by buttonTexts[relayIndex]
+                    OutlinedTextField(
+                        value = streamerUrlInput,
+                        onValueChange = {
+                            streamerUrlInput = it
+                            relay.streamerUrl = streamerUrlInput
+                            saveSettings()
+                        },
+                        label = { Text("Streamer URL") },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                    )
+                    OutlinedTextField(
+                        modifier = Modifier.padding(bottom = 15.dp),
+                        value = passwordInput,
+                        onValueChange = {
+                            passwordInput = it
+                            relay.password = passwordInput
+                            saveSettings()
+                        },
+                        label = { Text("Password") },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                    )
+                    Text(status)
+                    Button(
+                        modifier = Modifier.padding(10.dp),
+                        onClick = {
+                            if (!relaysStarted[relayIndex]) {
+                                buttonTexts[relayIndex].value = "Stop"
+                                start(relayIndex)
+                            } else {
+                                buttonTexts[relayIndex].value = "Start"
+                                stop(relayIndex)
+                            }
+                        },
+                    ) {
+                        Text(text)
                     }
                 }
-            ) {
-                Text(text)
             }
             Text("Version $version")
         }
--- app/src/main/java/com/eerimoq/moblink/Settings.kt
+++ app/src/main/java/com/eerimoq/moblink/Settings.kt
@@ -1,32 +1,46 @@
 package com.eerimoq.moblink
 
 import android.content.SharedPreferences
+import com.google.gson.Gson
 import java.util.UUID
 
-class Settings(private val sharedPreferences: SharedPreferences) {
+class RelaySettings {
     var streamerUrl = ""
     var password = ""
-    var relayId = ""
-    var name = ""
+}
+
+class Database {
+    var relayId = UUID.randomUUID().toString()
+    var name = "Relay"
+    var relays: List<RelaySettings> =
+        listOf(RelaySettings(), RelaySettings(), RelaySettings(), RelaySettings(), RelaySettings())
+}
+
+class Settings(private val sharedPreferences: SharedPreferences) {
+    var database = Database()
 
     init {
         load()
     }
 
-    fun load() {
-        streamerUrl = sharedPreferences.getString("streamerUrl", "") ?: ""
-        password = sharedPreferences.getString("password", "") ?: ""
-        val uuid = UUID.randomUUID().toString()
-        relayId = sharedPreferences.getString("relayId", uuid) ?: uuid
-        name = sharedPreferences.getString("name", "Relay") ?: "Relay"
+    private fun load() {
+        val value = sharedPreferences.getString("database", "{}") ?: "{}"
+        try {
+            database = Gson().fromJson(value, Database::class.java)
+        } catch (_: Exception) {
+            // Migrate from old database.
+            database.relays[0].streamerUrl = sharedPreferences.getString("streamerUrl", "") ?: ""
+            database.relays[0].password = sharedPreferences.getString("password", "") ?: ""
+            val uuid = UUID.randomUUID().toString()
+            database.relayId = sharedPreferences.getString("relayId", uuid) ?: uuid
+            database.name = sharedPreferences.getString("name", "Relay") ?: "Relay"
+            store()
+        }
     }
 
     fun store() {
         val editor = sharedPreferences.edit()
-        editor.putString("streamerUrl", streamerUrl)
-        editor.putString("password", password)
-        editor.putString("relayId", relayId)
-        editor.putString("name", name)
+        editor.putString("database", Gson().toJson(database))
         editor.apply()
     }
 }
